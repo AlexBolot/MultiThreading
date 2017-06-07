a7e2af11f4377abbfb564acc00f5e9aa2b0eb4a0
alexandre <bolotalex06@gmail.com>
2017-06-07 12:01:00 +0200

    Update FileScanner

--- src/main/java/multiThreading/seance6/FileScanner.java
+++ src/main/java/multiThreading/seance6/FileScanner.java
@@ -8,18 +8,23 @@ import java.util.concurrent.ArrayBlockingQueue;
  .
  . The FileScanner	 Class was Coded by : Alexandre BOLOT
  .
- . Last Modified : 07/06/17 10:25
+ . Last Modified : 07/06/17 11:59
  .
  . Contact : bolotalex06@gmail.com
  ...............................................................................................................................*/
 
+@SuppressWarnings({"InfiniteLoopStatement", "ConstantConditions"})
 public class FileScanner
 {
+    private final static String TXT = "txt";
+    private final static String PNG = "png";
+    
     public static void main (String argv[])
     {
-        final ArrayBlockingQueue<File> blockingQueue = new ArrayBlockingQueue<>(100);
+        final ArrayBlockingQueue<File> fileQueue = new ArrayBlockingQueue<>(100);
+        final ArrayBlockingQueue<Long> sizeQueue = new ArrayBlockingQueue<>(100);
         
-        Thread producer = new Thread()
+        Thread producerTxt = new Thread()
         {
             @Override
             public void run ()
@@ -28,12 +33,32 @@ public class FileScanner
                 
                 for (File file : folder.listFiles())
                 {
-                    if(file.isFile()) blockingQueue.offer(file);
+                    if(file.isFile() && isFormat(file, TXT))
+                    {
+                        fileQueue.offer(file);
+                    }
                 }
             }
         };
         
-        Thread consumer = new Thread()
+        Thread producerPng = new Thread()
+        {
+            @Override
+            public void run ()
+            {
+                File folder = new File("/Users/alexandre/Desktop/ToRead/");
+                
+                for (File file : folder.listFiles())
+                {
+                    if(file.isFile() && isFormat(file, PNG))
+                    {
+                        fileQueue.offer(file);
+                    }
+                }
+            }
+        };
+        
+        Thread consumerPrint = new Thread()
         {
             @Override
             public void run ()
@@ -42,17 +67,58 @@ public class FileScanner
                 {
                     try
                     {
-                        System.out.println(blockingQueue.take().getName());
+                        File file = fileQueue.take();
+                        System.out.println(file.getName());
+                        sizeQueue.offer(file.length());
                     }
-                    catch (InterruptedException ignored)
+                    catch (InterruptedException ie)
                     {
-                        //Nothing to do here
+                        ie.printStackTrace();
                     }
                 }
             }
         };
         
-        producer.start();
-        consumer.start();
+        Thread consumerSize = new Thread()
+        {
+            @Override
+            public void run ()
+            {
+                long totalSize = 0;
+                
+                while (true)
+                {
+                    try
+                    {
+                        long fileSize = sizeQueue.take();
+                        totalSize += fileSize;
+                        
+                        System.out.println("Total Size = " + totalSize + " octets");
+                    }
+                    catch (InterruptedException ie)
+                    {
+                        ie.printStackTrace();
+                    }
+                }
+            }
+        };
+        
+        producerTxt.start();
+        producerPng.start();
+        consumerPrint.start();
+        consumerSize.start();
+    }
+    
+    private static boolean isFormat (File file, String format)
+    {
+        String name = file.getName();
+        try
+        {
+            return name.substring(name.lastIndexOf(".") + 1).equals(format);
+        }
+        catch (Exception e)
+        {
+            return false;
+        }
     }
 }
